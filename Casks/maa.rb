b3a1810447498a099f219dcc17ee2561207e79e4
AbdulRahman Habeeb <74508174+w77sh@users.noreply.github.com>
2026-06-17 05:03:42 +0300

    fix(maa): update build number to 16 in url

--- Casks/maa.rb
+++ Casks/maa.rb
@@ -2,7 +2,7 @@ cask "maa" do
   version "1.0.13"
   sha256 "da6e9a641e12d702f574c3983f434ac2697b79d639bc1fb9e91d027d9b7ab958"
 
-  url "https://github.com/w77sh/Maa/releases/download/v#{version}/Maa-#{version}-14-macOS.dmg"
+  url "https://github.com/w77sh/Maa/releases/download/v#{version}/Maa-#{version}-16-macOS.dmg"
   name "Maa"
   desc "A beautiful and simple drink reminder app"
   homepage "https://github.com/w77sh/Maa"
